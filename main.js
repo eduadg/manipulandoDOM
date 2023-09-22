35035255df81a922ea6e5f0781d61611a59c1ff6
eduadg <eduadg@hotmail.com>
2023-09-22 11:54:06 -0300

    Primeiro commit

---
+++ main.js
@@ -0,0 +1,36 @@
+const Focobt = document.querySelector(".app__card-button--foco");
+const Curtobt = document.querySelector(".app__card-button--curto");
+const Longobt = document.querySelector(".app__card-button--longo");
+const html = document.querySelector('html')
+const titulo = document.querySelector('.app__title')
+const imagem = document.querySelector('.app__image')
+
+
+Focobt.addEventListener('click', () => {
+    MudaContexto('foco')
+})
+
+Curtobt.addEventListener('click', () => {
+    MudaContexto('descanso-curto')
+})
+
+Longobt.addEventListener('click', () => {
+    MudaContexto('descanso-longo')
+})
+
+function MudaContexto(contexto) {
+    html.setAttribute('data-contexto', contexto);
+    imagem.setAttribute('src', `/imagens/${contexto}.png`)
+    switch (contexto) {
+        case'foco':
+            titulo.innerHTML = `Otimize sua produtividade,<br> <strong class="app__title-strong">mergulhe no que importa.</strong>`
+            break;
+
+        case 'curto' :   
+            titulo.innerHTML = `Que tal dar uma respirada? <br> <strong class="app__title-strong">Faça uma pausa curta!</strong>`
+        default:
+
+
+            break;
+    }
+}
