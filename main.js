a332b59af363e2c96ecfd46676359bf6e018b2b9
Eduardo Aguiar de Godoi <eduadg@hotmail.com>
2023-09-24 04:14:36 -0300

    feat: Adiciona alternância para música de fundo e melhora ativação de botão

--- main.js
+++ main.js
@@ -4,21 +4,41 @@ const Longobt = document.querySelector(".app__card-button--longo");
 const html = document.querySelector('html')
 const titulo = document.querySelector('.app__title')
 const imagem = document.querySelector('.app__image')
+const botoes = document.querySelectorAll('.app__card-button');
+const focoMusic = document.querySelector('#alternar-musica')
+const music = new Audio('/sons/luna-rise-part-one.mp3')
 
+music.loop = true;
+
+focoMusic.addEventListener('change', () => {
+    if (music.paused) {
+        music.play()
+    } else {
+        music.pause()
+    }
+})
 
 Focobt.addEventListener('click', () => {
     MudaContexto('foco')
+    Focobt.classList.add('active')
 })
 
 Curtobt.addEventListener('click', () => {
     MudaContexto('descanso-curto')
+    Curtobt.classList.add('active')
 })
 
 Longobt.addEventListener('click', () => {
     MudaContexto('descanso-longo')
+    Longobt.classList.add('active')
 })
 
 function MudaContexto(contexto) {
+
+    botoes.forEach(function (botao) {
+        botao.classList.remove('active');
+    })
+
     html.setAttribute('data-contexto', contexto);
     imagem.setAttribute('src', `/imagens/${contexto}.png`)
     switch (contexto) {
@@ -29,14 +49,17 @@ function MudaContexto(contexto) {
         case 'descanso-curto' :   
             titulo.innerHTML = `Que tal dar uma respirada? <br> <strong class="app__title-strong">Faça uma pausa curta!</strong>`
        
+        break;
+
         case 'descanso-longo' : 
         
         titulo.innerHTML  =  `Hora de voltar à superfície.
         <br> <strong class="app__title-strong">Faça uma pausa longa!</strong>`
        
-            default:
+        break;
 
-
-            break;
+        default:   
     }
 }
+
+
