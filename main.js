c57105a4c9d4074499f04055dff2eeb102284e78
Eduardo Aguiar de Godoi <eduadg@hotmail.com>
2023-09-26 01:55:42 -0300

    feat: criando o cronometro

--- main.js
+++ main.js
@@ -1,65 +1,99 @@
 const Focobt = document.querySelector(".app__card-button--foco");
 const Curtobt = document.querySelector(".app__card-button--curto");
 const Longobt = document.querySelector(".app__card-button--longo");
-const html = document.querySelector('html')
-const titulo = document.querySelector('.app__title')
-const imagem = document.querySelector('.app__image')
-const botoes = document.querySelectorAll('.app__card-button');
-const focoMusic = document.querySelector('#alternar-musica')
-const music = new Audio('/sons/luna-rise-part-one.mp3')
+const html = document.querySelector("html");
+const titulo = document.querySelector(".app__title");
+const imagem = document.querySelector(".app__image");
+const botoes = document.querySelectorAll(".app__card-button");
+const startPausebt = document.querySelector("#start-pause");
 
-music.loop = true;
+let TempoEmSegundos = 5;
+let intervaloTemporizador = null;
 
-focoMusic.addEventListener('change', () => {
-    if (music.paused) {
-        music.play()
-    } else {
-        music.pause()
-    }
-})
+const musicaFoco = document.querySelector("#alternar-musica");
+const musica = new Audio("/sons/luna-rise-part-one.mp3");
 
-Focobt.addEventListener('click', () => {
-    MudaContexto('foco')
-    Focobt.classList.add('active')
-})
+musicaFoco.addEventListener("change", () => {
+  if (musica.paused) {
+    musica.play();
+  } else {
+    musica.pause();
+  }
+});
 
-Curtobt.addEventListener('click', () => {
-    MudaContexto('descanso-curto')
-    Curtobt.classList.add('active')
-})
+Focobt.addEventListener("click", () => {
+  MudaContexto("foco");
+  Focobt.classList.add("active");
+});
 
-Longobt.addEventListener('click', () => {
-    MudaContexto('descanso-longo')
-    Longobt.classList.add('active')
-})
+Curtobt.addEventListener("click", () => {
+  MudaContexto("descanso-curto");
+  Curtobt.classList.add("active");
+});
+
+Longobt.addEventListener("click", () => {
+  MudaContexto("descanso-longo");
+  Longobt.classList.add("active");
+});
 
 function MudaContexto(contexto) {
+  botoes.forEach((botao) => {
+    botao.classList.remove("active");
+  });
 
-    botoes.forEach(function (botao) {
-        botao.classList.remove('active');
-    })
+  html.setAttribute("data-contexto", contexto);
+  imagem.setAttribute("src", `/imagens/${contexto}.png`);
+  switch (contexto) {
+    case "foco":
+      titulo.innerHTML = `Otimize sua produtividade,<br> <strong class="app__title-strong">mergulhe no que importa.</strong>`;
+      break;
 
-    html.setAttribute('data-contexto', contexto);
-    imagem.setAttribute('src', `/imagens/${contexto}.png`)
-    switch (contexto) {
-        case'foco':
-            titulo.innerHTML = `Otimize sua produtividade,<br> <strong class="app__title-strong">mergulhe no que importa.</strong>`
-            break;
+    case "descanso-curto":
+      titulo.innerHTML = `Que tal dar uma respirada? <br> <strong class="app__title-strong">Faça uma pausa curta!</strong>`;
 
-        case 'descanso-curto' :   
-            titulo.innerHTML = `Que tal dar uma respirada? <br> <strong class="app__title-strong">Faça uma pausa curta!</strong>`
-       
-        break;
+      break;
 
-        case 'descanso-longo' : 
-        
-        titulo.innerHTML  =  `Hora de voltar à superfície.
-        <br> <strong class="app__title-strong">Faça uma pausa longa!</strong>`
-       
-        break;
+    case "descanso-longo":
+      titulo.innerHTML = `Hora de voltar à superfície.
+        <br> <strong class="app__title-strong">Faça uma pausa longa!</strong>`;
 
-        default:   
-    }
+      break;
+
+    default:
+  }
 }
+
+const temporizador = () => {
+    if (TempoEmSegundos <= 0) {
+        alert('seu tempo acabou!')
+        TempoEmSegundos = 5;
+        zerar();
+        return;
+    }
+    
+    TempoEmSegundos -= 1;
+    console.log('tempo: ' + TempoEmSegundos);
+}
+
+startPausebt.addEventListener('click' , iniciarContagem)
+
+function iniciarContagem() {
+    if (intervaloTemporizador) {
+        temporizador
+        zerar();
+        return;
+    }
+    
+    intervaloTemporizador = setInterval(temporizador, 1000);
+    
+}
+
+function zerar() {
+    clearInterval(intervaloTemporizador);
+    intervaloTemporizador=null;
+    return;
+}
+
+
 
 
